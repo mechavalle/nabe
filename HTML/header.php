e5deb170b7e22f8cb7373a96c73bf17527966bb4
cristinalg <cristina_13logan@hotmail.com>
2018-12-10 10:20:07 -0600

    footer, header, index, navbar

---
+++ HTML/header.php
@@ -0,0 +1,30 @@
+<!--eof topline-->
+	<section class="page_toplogo ls s-py-15 text-center">
+		<div class="container-fluid">
+			<div class="row align-items-center">
+				<div class="col-lg-4">
+					<div class="d-lg-flex justify-content-lg-end align-items-lg-center">
+						<span class="social-icons top">
+							<a href="#" class="fa fa-facebook " title="facebook"></a>
+							<a href="#" class="fa fa-twitter " title="twitter"></a>
+							<a href="#" class="fa fa-google " title="google"></a>
+							<a href="#" class="fa fa-linkedin " title="linkedin"></a>
+						</span>
+					</div>
+				</div>
+				<div class="col-lg-4 text-center">
+					<div class="text-center">
+						<div class="header_logo_center">
+							<a href="./" class="logo">
+								<img src="images/logo.png" alt="">
+							</a>
+						</div>
+						<!-- eof .header_left_logo -->
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<button class="btn-outline-darkgrey  d-none d-lg-block">¿Buscas personal?</button>
+				</div>
+			</div>
+		</div>
+	</section>
